c5af6660bde4fb2ee67e9dae78f63ba2b6ea18cd
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2024-04-07 02:45:49 -0300

    test: order status

---
+++ src/pages/app/orders/status.spec.tsx
@@ -0,0 +1,48 @@
+import { render } from "@testing-library/react";
+
+import Status from "./status";
+
+describe("Order Status", () => {
+  it("should display the right test and class based on the order status", () => {
+    // Pending
+    const wrapper = render(<Status status="pending" />);
+
+    const statusText = wrapper.getByText("Pending");
+    const badge = wrapper.getByTestId("badge");
+
+    expect(statusText).toBeVisible();
+    expect(badge).toHaveClass("bg-slate-400");
+
+    // Canceled
+    wrapper.rerender(<Status status="canceled" />);
+    const canceledStatusText = wrapper.getByText("Canceled");
+    const canceledBadge = wrapper.getByTestId("badge");
+
+    expect(canceledStatusText).toBeVisible();
+    expect(canceledBadge).toHaveClass("bg-rose-500");
+
+    // Processing
+    wrapper.rerender(<Status status="processing" />);
+    const processingStatusText = wrapper.getByText("Processing");
+    const processingBadge = wrapper.getByTestId("badge");
+
+    expect(processingStatusText).toBeVisible();
+    expect(processingBadge).toHaveClass("bg-yellow-500");
+
+    // Delivering
+    wrapper.rerender(<Status status="delivering" />);
+    const deliveringStatusText = wrapper.getByText("Delivering");
+    const deliveringBadge = wrapper.getByTestId("badge");
+
+    expect(deliveringStatusText).toBeVisible();
+    expect(deliveringBadge).toHaveClass("bg-amber-500");
+
+    // Delivered
+    wrapper.rerender(<Status status="delivered" />);
+    const deliveredStatusText = wrapper.getByText("Delivered");
+    const deliveredBadge = wrapper.getByTestId("badge");
+
+    expect(deliveredStatusText).toBeVisible();
+    expect(deliveredBadge).toHaveClass("bg-emerald-500");
+  });
+});
--- src/pages/app/orders/status.tsx
+++ src/pages/app/orders/status.tsx
@@ -5,35 +5,50 @@ function Status({ status }: { status: TStatus }) {
     case "pending":
       return (
         <>
-          <div className="h-2 w-2 rounded-full bg-slate-400" />
+          <div
+            data-testid="badge"
+            className="h-2 w-2 rounded-full bg-slate-400"
+          />
           <span>Pending</span>
         </>
       );
     case "canceled":
       return (
         <>
-          <div className="h-2 w-2 rounded-full bg-rose-500" />
+          <div
+            data-testid="badge"
+            className="h-2 w-2 rounded-full bg-rose-500"
+          />
           <span>Canceled</span>
         </>
       );
     case "processing":
       return (
         <>
-          <div className="h-2 w-2 rounded-full bg-yellow-500" />
+          <div
+            data-testid="badge"
+            className="h-2 w-2 rounded-full bg-yellow-500"
+          />
           <span>Processing</span>
         </>
       );
     case "delivering":
       return (
         <>
-          <div className="h-2 w-2 rounded-full bg-amber-500" />
+          <div
+            data-testid="badge"
+            className="h-2 w-2 rounded-full bg-amber-500"
+          />
           <span>Delivering</span>
         </>
       );
     case "delivered":
       return (
         <>
-          <div className="h-2 w-2 rounded-full bg-emerald-500" />
+          <div
+            data-testid="badge"
+            className="h-2 w-2 rounded-full bg-emerald-500"
+          />
           <span>Delivered</span>
         </>
       );
