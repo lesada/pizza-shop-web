3b581ceee9a2d35365581c45dffd8e9017e87821
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2024-04-08 23:09:05 -0300

    test: actions mock

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -2,6 +2,10 @@ import { setupWorker } from "msw/browser";
 
 import { env } from "@/env";
 
+import { approveOrderMock } from "./approve-order-mock";
+import { cancelOrderMock } from "./cancel-order-mock";
+import { deliverOrderMock } from "./deliver-order-mock";
+import { dispatchOrderMock } from "./dispatch-order-mock";
 import { getDailyRevenueInPeriodMock } from "./get-daily-revenue-mock";
 import { getDayOrdersAmountMock } from "./get-day-orders-amount-mock";
 import { getManagedRestaurantsMock } from "./get-managed-restaurant-mock";
@@ -30,7 +34,11 @@ export const worker = setupWorker(
   getProfileMock,
   updateProfileMock,
   getOrdersMock,
-  getOrderDetailsMock
+  getOrderDetailsMock,
+  approveOrderMock,
+  cancelOrderMock,
+  dispatchOrderMock,
+  deliverOrderMock
 );
 
 export async function enableMSW() {
